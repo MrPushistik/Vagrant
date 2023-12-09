bd9d2fc3e011ec531cb51a0de8c69955792eeb52
alex <345w2011@mail.ru>
2023-12-09 21:57:37 +0400

    fix VPN-install

--- VPN-install.sh
+++ VPN-install.sh
@@ -1,7 +1,7 @@
 wget https://repo.protonvpn.com/fedora-39-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.1-2.noarch.rpm
 
-sudo dnf -y install ./protonvpn-stable-release-1.0.1-2.noarch.rpm
+sudo dnf install -y ./protonvpn-stable-release-1.0.1-2.noarch.rpm
 
 sudo dnf check-update && dnf upgrade
 
-sudo -y dnf install --refresh proton-vpn-gnome-desktop
+sudo dnf install -y --refresh proton-vpn-gnome-desktop
